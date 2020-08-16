0ff6dd12a8063d23115c51f93a7faef1ecc5ef76
AlexanderLuther <alexanderluther08@gmail.com>
2020-08-16 13:31:30 -0600

    Inicio del proyecto

---
+++ src/main/java/com/hluther/controlClasses/AnalysisDriver.java
@@ -0,0 +1,23 @@
+package com.hluther.controlClasses;
+
+//import com.hluther.parser.ParseException;
+//import com.hluther.parser.Parser;
+import com.hluther.gui.LCompilerFrame;
+import java.io.StringReader;
+/**
+ *
+ * @author helmuth
+ */
+public class AnalysisDriver {
+    
+    public void doAnalysis(String text, LCompilerFrame principalFrame){
+  /*      try {
+            Parser parser = new Parser(new StringReader(text));
+            parser.setPrincipalFrame(principalFrame);
+            parser.start();
+        } catch (ParseException ex) {
+            System.out.println("Error al analizar.");
+        }*/
+    }
+   
+}
---
+++ src/main/java/com/hluther/lcompiler/LCompiler.java
@@ -0,0 +1,13 @@
+package com.hluther.lcompiler;
+import com.hluther.gui.LCompilerFrame;
+/**
+ *
+ * @author helmuth
+ */
+public class LCompiler {
+    
+    public static void main(String[] args) {
+        LCompilerFrame lCompilerFrame = new LCompilerFrame();
+        lCompilerFrame.setVisible(true);
+    }
+}
